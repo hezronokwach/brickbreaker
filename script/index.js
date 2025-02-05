65c7dc7f36fe9f8fcea1407dedff5c25d614f97d
hezronokwach <hezronokwach@gmail.com>
2025-02-05 12:26:29 +0300

    Add ball class to hold properties of the ball

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
+import Ball from './ball.js';
 
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
@@ -8,9 +9,9 @@ const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 new InputHandler(paddle)
+let ball = new Ball(); 
 paddle.draw(ctx);
 let lastTime = 0;
-let img_ball = document.getElementById('imageBall');
 
 function gameLoop(time){
     let deltaTime = time - lastTime;
@@ -18,7 +19,7 @@ function gameLoop(time){
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
-    ctx.drawImage(img_ball,10,10,16,16);
+    ball.draw(ctx);
     requestAnimationFrame(gameLoop);
 }
 gameLoop();
