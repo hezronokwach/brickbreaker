967d7cbb3de94f7c2501d05e1ad1cd3b86c627ee
hezronokwach <hezronokwach@gmail.com>
2025-02-05 16:39:37 +0300

    Add bricks

--- script/game.js
+++ script/game.js
@@ -1,6 +1,7 @@
 import Paddle from './paddle.js';
 import InputHandler from './input.js';
 import Ball from './ball.js';
+import Brick from './brick.js'
 
 export default class Game{
     constructor(gamewidth, gameheight){
@@ -10,14 +11,14 @@ export default class Game{
     start(){
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
+        this.gameObjects = [this.ball, this.paddle];
         new InputHandler(this.paddle);
     }    
     update(deltaTime){
-        this.paddle.update(deltaTime);
-       this.ball.update(deltaTime);
+       this.gameObjects.forEach(object => object.update(deltaTime))
     }
     draw(ctx){
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
+        this.gameObjects.forEach(object => object.draw(ctx))
+
     }
 }
