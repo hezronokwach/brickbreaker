ffb07d11a68cd5470014d3ecfd2a2ff5a4cf8fdd
hezronokwach <hezronokwach@gmail.com>
2025-02-05 10:31:17 +0300

    Draw paddle in the canvas

--- script/index.js
+++ script/index.js
@@ -1,6 +1,11 @@
+import Paddle from './paddle.js';
+
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
 
-// Example: Draw a rectangle
-ctx.fillStyle = '#f00';
-ctx.fillRect(20, 20, 100, 100);
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(ctx);
