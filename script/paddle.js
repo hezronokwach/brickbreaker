338f69e80631182449611a9af0b52fd4058d07dd
hezronokwach <hezronokwach@gmail.com>
2025-02-05 10:36:22 +0300

    Add gameloop to move the paddle

--- script/paddle.js
+++ script/paddle.js
@@ -11,7 +11,10 @@ export default class Paddle{
     draw(ctx){
         ctx.fillStyle = '#0ff';
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-
+    }
+    update(deltaTime){
+        if(!deltaTime) return;
+        this.position.x += 5/deltaTime;
     }
 
 }
