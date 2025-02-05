0e11c624b64cce4e08406d1a8ac6d0af15925507
hezronokwach <hezronokwach@gmail.com>
2025-02-05 14:29:37 +0300

    refactor: Add class game to hold the properties of the game

--- script/index.js
+++ script/index.js
@@ -1,26 +1,20 @@
-import Paddle from './paddle.js';
-import InputHandler from './input.js';
-import Ball from './ball.js';
+import Game from "./game.js"
 
 let canvas = document.getElementById('gameCanvas');
 let ctx = canvas.getContext('2d');
 
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
-let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-new InputHandler(paddle)
-let ball = new Ball(GAME_WIDTH,GAME_HEIGHT); 
-paddle.draw(ctx);
+let game = new Game(GAME_WIDTH, GAME_HEIGHT);
+game.start();
 let lastTime = 0;
 
 function gameLoop(time){
     let deltaTime = time - lastTime;
     lastTime = time;
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    paddle.update(deltaTime);
-    paddle.draw(ctx);
-    ball.update(deltaTime);
-    ball.draw(ctx);
+    game.update(deltaTime);
+    game.draw(ctx);
     requestAnimationFrame(gameLoop);
 }
 requestAnimationFrame(gameLoop);
