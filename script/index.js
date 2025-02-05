4aaa6093ab75ac2d63c8ebac09ce979bd548f395
hezronokwach <hezronokwach@gmail.com>
2025-02-05 10:36:22 +0300

    Add gameloop to move the paddle

--- script/index.js
+++ script/index.js
@@ -9,3 +9,14 @@ ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 paddle.draw(ctx);
+let lastTime = 0;
+
+function gameLoop(time){
+    let deltaTime = time - lastTime;
+    lastTime = time;
+    ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+    requestAnimationFrame(gameLoop);
+}
+gameLoop();
