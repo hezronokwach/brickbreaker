0ae6a06459d19901fdef74d4eb3a9a35e1d799c8
hezronokwach <hezronokwach@gmail.com>
2025-02-05 12:21:54 +0300

    Add ball image

--- script/index.js
+++ script/index.js
@@ -10,6 +10,7 @@ let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 new InputHandler(paddle)
 paddle.draw(ctx);
 let lastTime = 0;
+let img_ball = document.getElementById('imageBall');
 
 function gameLoop(time){
     let deltaTime = time - lastTime;
@@ -17,6 +18,7 @@ function gameLoop(time){
     ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
     paddle.update(deltaTime);
     paddle.draw(ctx);
+    ctx.drawImage(img_ball,10,10,16,16);
     requestAnimationFrame(gameLoop);
 }
 gameLoop();
