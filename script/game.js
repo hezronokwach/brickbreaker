68935dc0e9f9f83b84a9db056b46afe972331275
hokwach <hezronokwach@gmail.com>
2025-02-06 22:18:23 +0300

    Add level 2 to the game

--- script/game.js
+++ script/game.js
@@ -2,13 +2,14 @@ import Paddle from './paddle.js';
 import InputHandler from './input.js';
 import Ball from './ball.js';
 import Brick from './brick.js'
-import { level1, buildLevel } from './levels.js'
+import { level1, buildLevel , level2} from './levels.js'
 
 const GAMESTATE = {
     PAUSE: 0,
     PLAY: 1,
     MENU: 2,
-    OVER: 3
+    OVER: 3,
+    NEWLEVEL: 4
 };
 export default class Game {
     constructor(gamewidth, gameheight) {
@@ -18,25 +19,37 @@ export default class Game {
         this.ball = new Ball(this);
         this.paddle = new Paddle(this);
         this.gameObjects = [];
+        this.bricks = [];
+        this.lives = 2;
+        this.levels = [level1, level2];
+        this.currentLevel = 0;
         new InputHandler(this.paddle, this);
 
     }
     start() {
-        if (this.gamestate !== GAMESTATE.MENU) return;  
+        if (this.gamestate !== GAMESTATE.MENU && this.gamestate !== GAMESTATE.NEWLEVEL) return;  
 
-        let bricks = buildLevel(this, level1);
+        this.bricks = buildLevel(this, this.levels[this.currentLevel]);
+        this.ball.reset();
 
-        this.gameObjects = [this.ball, this.paddle, ...bricks];
+        this.gameObjects = [this.ball, this.paddle];
         this.gamestate = GAMESTATE.PLAY
     }
     update(deltaTime) {
-        if (this.gamestate === GAMESTATE.PAUSE || this.gamestate === GAMESTATE.MENU) return;
+        if (this.lives === 0) this.gamestate = GAMESTATE.OVER
 
-        this.gameObjects.forEach(object => object.update(deltaTime))
-        this.gameObjects = this.gameObjects.filter(object => !object.delete)
+        if (this.gamestate === GAMESTATE.PAUSE || this.gamestate === GAMESTATE.MENU || this.gamestate === GAMESTATE.OVER) return;
+        if (this.bricks.length === 0) {
+            this.currentLevel++;
+            this.gamestate = GAMESTATE.NEWLEVEL;
+            this.start();
+        }
+
+        [...this.gameObjects, ...this.bricks].forEach(object => object.update(deltaTime))
+        this.bricks = this.bricks.filter(brick => !brick.delete)
     }
     draw(ctx) {
-        this.gameObjects.forEach(object => object.draw(ctx))
+        [...this.gameObjects, ...this.bricks].forEach(object => object.draw(ctx))
         if (this.gamestate == GAMESTATE.PAUSE) {    
         ctx.rect(0, 0, this.gamewidth, this.gameheight)
         ctx.fillStyle = "rgba(0, 0, 0,0.5)";
@@ -56,6 +69,16 @@ export default class Game {
             ctx.textAlign = "center";
             ctx.fillText("PRESS SPACE TO START", this.gamewidth / 2, this.gameheight / 2);
             }
+
+            if (this.gamestate == GAMESTATE.OVER) {    
+                ctx.rect(0, 0, this.gamewidth, this.gameheight)
+                ctx.fillStyle = "rgba(0, 0, 0,1)";
+                ctx.fill()
+                ctx.font = "30px Arial";
+                ctx.fillStyle = "#fff";
+                ctx.textAlign = "center";
+                ctx.fillText("GAME OVER", this.gamewidth / 2, this.gameheight / 2);
+                }
 
     }
     pause() {
