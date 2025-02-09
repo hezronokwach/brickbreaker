fb0fd30ae1e71182952022c75352d1d7d6ec8dd8
hokwach <hezronokwach@gmail.com>
2025-02-09 15:37:11 +0300

    Refactor to use DOM elements instead of canvas

--- script/brick.js
+++ script/brick.js
@@ -1,24 +1,29 @@
 import { detectCollision } from "./detectCollision.js";
+
 export default class Brick {
     constructor(game, position) {
-        this.image = document.getElementById('imageBrick');
-        this.position = position
+        this.game = game;
+        this.position = position;
         this.width = 80;
         this.height = 24;
-        this.game = game;
-
         this.delete = false;
+        this.element = document.createElement('div');
+        this.element.className = 'brick';
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+        this.game.gameContainer.appendChild(this.element); // Use this.game.gameContainer
     }
+
+    draw() {
+        this.element.style.left = `${this.position.x}px`;
+        this.element.style.top = `${this.position.y}px`;
+    }
+
     update() {
         if (detectCollision(this.game.ball, this)) {
             this.game.ball.speed.y = -this.game.ball.speed.y;
             this.delete = true;
+            this.element.remove();
         }
-    }
-    draw(ctx) {
-        ctx.drawImage(
-            this.image, this.position.x, this.position.y, this.width, this.height
-        )
-
     }
 }
