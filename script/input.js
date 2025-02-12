cc32ec4b035abade31724bbfaea53420cc0d9836
hokwach <hezronokwach@gmail.com>
2025-02-12 22:00:53 +0300

    fix: Fix tokenization of the player

--- script/input.js
+++ script/input.js
@@ -1,30 +1,52 @@
+export default class InputHandler {
+    constructor(paddle, game) {
+        this.paddle = paddle;
+        this.game = game;
+        
+        // Store bound handlers to be able to remove them
+        this.keyDownHandler = this.handleKeyDown.bind(this);
+        this.keyUpHandler = this.handleKeyUp.bind(this);
+        
+        this.initializeControls();
+    }
 
-export default class InputHandler{
-    constructor(paddle,game){
-        document.addEventListener('keydown', (event)=>{
-            switch(event.keyCode){
-                case 37:
-                    paddle.moveLeft();                  
-                    break;
-                case 39:
-                    paddle.moveRight();
-                    break;
-               
-                case 32:
-                        game.start();
-                
-                    break;
-            }
-        });
-        document.addEventListener('keyup', (event)=>{
-            switch(event.keyCode){
-                case 37:
-                    if(paddle.speed < 0) paddle.stop();             
-                    break;
-                case 39:
-                    if(paddle.speed > 0) paddle.stop();
-                    break;
-            }
-        });
+    initializeControls() {
+        // Remove existing listeners if any
+        document.removeEventListener('keydown', this.keyDownHandler);
+        document.removeEventListener('keyup', this.keyUpHandler);
+        
+        // Add new listeners
+        document.addEventListener('keydown', this.keyDownHandler);
+        document.addEventListener('keyup', this.keyUpHandler);
+    }
+
+    handleKeyDown(event) {
+        switch(event.keyCode) {
+            case 37:
+                this.paddle.moveLeft();
+                break;
+            case 39:
+                this.paddle.moveRight();
+                break;
+            case 32:
+                this.game.start();
+                break;
+        }
+    }
+
+    handleKeyUp(event) {
+        switch(event.keyCode) {
+            case 37:
+                if(this.paddle.speed < 0) this.paddle.stop();
+                break;
+            case 39:
+                if(this.paddle.speed > 0) this.paddle.stop();
+                break;
+        }
+    }
+
+    destroy() {
+        document.removeEventListener('keydown', this.keyDownHandler);
+        document.removeEventListener('keyup', this.keyUpHandler);
     }
 }
