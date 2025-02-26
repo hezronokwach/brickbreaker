9c0bed1dba671a4248f666b7c81830802eef55d7
jamos <amosjoel91@gmail.com>
2025-02-26 18:14:08 +0300

    feat: add basic HTTP server to serve static files

---
+++ script/server.js
@@ -0,0 +1,27 @@
+import { readFile } from 'fs/promises';
+import { createServer } from 'http';
+import { extname } from 'path';
+
+const server = createServer(async (req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './index.html';
+    }
+
+    const ext = String(extname(filePath)).toLowerCase();
+
+    const contentType = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css'
+    }[ext] || 'application/octet-stream';
+
+    try {
+        const data = await readFile(filePath);
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data, 'utf-8');
+    } catch {
+        res.writeHead(404);
+        res.end('404 Not Found');
+    }
+});
