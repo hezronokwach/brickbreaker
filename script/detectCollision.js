274dbd4a44855fc70466230896080e6ea3fa03a4
Murzuqisah <amosjoel229@gmail.com>
2025-02-27 01:18:48 +0300

    feat: enhance collision detection by adding deltaTime parameter and refining calculations

--- script/detectCollision.js
+++ script/detectCollision.js
@@ -1,4 +1,9 @@
-export function detectCollision(ball, gameObject) {
+export function detectCollision(ball, gameObject, deltaTime) {
+    let ballNextX = ball.position.x + ball.speed.x * (deltaTime / 16.67);
+    let ballNextY = ball.position.y + ball.speed.y * (deltaTime / 16.67);
+
+    let leftOfBall = ballNextX;
+    let rightOfBall = ballNextX + ball.size;
     let bottomOfBall = ball.position.y + ball.size;
     let topOfBall = ball.position.y;
 
@@ -6,6 +11,18 @@ export function detectCollision(ball, gameObject) {
     let leftSideOfObject = gameObject.position.x;
     let rightSideOfObject = gameObject.position.x + gameObject.width;
     let bottomOfObject = gameObject.position.y + gameObject.height;
+
+    let collisionTimeX = null;
+    let collisionTimeY = null;
+
+    // check collision on x-axis
+    if (rightOfBall >= leftSideOfObject && leftOfBall <= rightSideOfObject) {
+        if (ball.speed.y > 0 && bottomOfBall >= topOfObject && topOfBall <= topOfObject) {
+            collisionTimeY = (topOfObject - bottomOfBall) / ball.speed.y;
+        } else if (ball.speed.y < 0 && topOfBall <= bottomOfObject && bottomOfBall >= bottomOfBall) {
+            collisionTimeY = (bottomOfObject - topOfBall) / ball.speed.y;
+        }
+    }
 
     if (
         bottomOfBall >= topOfObject &&
@@ -24,6 +41,6 @@ export function detectCollision(ball, gameObject) {
             ball.speed.x = -ball.speed.x;
         }
         return true;
-    } 
+    }
     return false;
 }
